daba5bbd2d372af4b6ff267206ab55853ac1e9a9
Matthew Li <mtthw.li7@gmail.com>
2021-09-20 19:14:33 -0400

    create csv files

--- main/main.py
+++ main/main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
 # create full data file
-data = pd.read_csv('/Users/matthewli/Downloads/instacart/orders.csv')
-products = pd.read_csv('/Users/matthewli/Downloads/instacart/products.csv')
-departments = pd.read_csv('/Users/matthewli/Downloads/instacart/departments.csv')
-items = pd.read_csv('/Users/matthewli/Downloads/instacart/order_products__prior.csv')
-items2 = pd.read_csv('/Users/matthewli/Downloads/instacart/order_products__train.csv')
+data = pd.read_csv("/Users/matthewli/Downloads/instacart/orders.csv")
+products = pd.read_csv("/Users/matthewli/Downloads/instacart/products.csv")
+departments = pd.read_csv("/Users/matthewli/Downloads/instacart/departments.csv")
+items = pd.read_csv("/Users/matthewli/Downloads/instacart/order_products__prior.csv")
+items2 = pd.read_csv("/Users/matthewli/Downloads/instacart/order_products__train.csv")
 items = items.append(items2, ignore_index=True)
+print("done")
 
-test = pd.merge(items, products, on='product_id')
-test = pd.merge(test, departments, on='department_id')
-test = pd.merge(test, data, on='order_id')
-test = test.drop(['aisle_id', 'eval_set', 'order_dow', 'days_since_prior_order'], axis=1)
-test.to_csv('test.csv', index=False)
+total = pd.merge(items, products, on="product_id")
+total = pd.merge(total, departments, on="department_id")
+total = pd.merge(total, data, on="order_id")
+total = total.drop(
+    ["aisle_id", "eval_set", "order_dow", "days_since_prior_order"], axis=1
+)
+total.to_csv("total.csv", index=False)
+test = total.sample(n=500, replace=False)
+test.to_csv("test.csv", index=False)
