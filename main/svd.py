5c9e1f50be17cd2a77e654a2eb63a54e31c78edc
Matthew Li <mtthw.li7@gmail.com>
2021-09-23 15:40:39 -0400

    create dataframe for svd

--- main/svd.py
+++ main/svd.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import scipy as sp
+import numpy as np
 from sklearn.decomposition import TruncatedSVD
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -31,11 +32,12 @@ print(len(data.product_name.unique()))
 # plt.show()
 
 # SURPRISE METHOD
+# Item x Item
 df = data.drop(
     [
         "product_id",
         "add_to_cart_order",
-        "product_name",
+        "department",
         "department_id",
         "user_id",
         "order_number",
@@ -43,16 +45,45 @@ df = data.drop(
     ],
     axis=1,
 )
-df["reordered"] += 1
-print(df)
-reader = Reader(rating_scale=(1, 5))
-data = Dataset.load_from_df(df[["order_id", "department", "reordered"]], reader)
-# algo = SVD()
-# cross_validate(algo, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
-param_grid = {"n_epochs": [5, 10], "lr_all": [0.002, 0.005], "reg_all": [0.4, 0.6]}
-gs = GridSearchCV(SVD, param_grid, measures=["rmse", "mae"], cv=3)
-gs.fit(data)
-print(gs.best_score["rmse"])
+df = df.groupby(by="order_id")
+list = []
+for group in df:
+    temp = group[1]
+    x = (
+        pd.merge(group[1].assign(key=1), group[1].assign(key=1), on="order_id")
+        .query("product_name_x < product_name_y")
+        .drop(columns=["order_id"])
+    )
+    list.append(x)
+total = pd.concat(list)
 
-# combination of parameters that gave the best RMSE score
-print(gs.best_params["rmse"])
+total["match"] = total.apply(
+    lambda row: min(row["reordered_x"], row["reordered_y"]), axis=1
+)
+total["match"] += 1
+total = total.drop(
+    [
+        "reordered_x",
+        "aisle_x",
+        "key_x",
+        "reordered_y",
+        "aisle_y",
+        "key_y",
+    ],
+    axis=1,
+)
+print(total)
+print(total.sort_values("match"))
+reader = Reader(rating_scale=(1, 5))
+data = Dataset.load_from_df(
+    total[["product_name_x", "product_name_y", "match"]], reader
+)
+algo = SVD()
+cross_validate(algo, data, measures=["RMSE", "MAE"], cv=5, verbose=True)
+# param_grid = {"n_epochs": [5, 10], "lr_all": [0.002, 0.005], "reg_all": [0.4, 0.6]}
+# gs = GridSearchCV(SVD, param_grid, measures=["rmse", "mae"], cv=3)
+# gs.fit(data)
+# print(gs.best_score["rmse"])
+
+# # combination of parameters that gave the best RMSE score
+# print(gs.best_params["rmse"])
